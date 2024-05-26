fb0f0442a13afe7355233681c46e2205b68f3149
Wishes38 <serkanozdemir38080@gmail.com>
2024-05-27 02:04:24 +0300

    commit

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Dtos.ProductDetailDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public interface IProductImageService
+    {
+        Task<List<ResultProductImageDto>> GetAllProductImageAsync();
+        Task CreateProductImageAsync(CreateProductImageDto createProductImageDto);
+        Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
+        Task DeleteProductImageAsync(string id);
+        Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+    }
+}
